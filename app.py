179c4c62ab09402fbf8401dbc2ac088d957bdfee
Arnold Gihozo <49104779+ArnoldGihozo@users.noreply.github.com>
2023-02-11 21:11:54 -0700

    progresssss :fire:

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
    return render_template("index.html")
+
+@app.route('/elements', methods=['GET', 'POST'])
+def about():
+   return render_template("elements.html")
